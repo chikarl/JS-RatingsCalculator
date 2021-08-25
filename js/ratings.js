0eaf6f6feaa996001dd473b006e23b947c65561f
chi karl <kchi8573@gmail.com>
2021-08-25 12:55:00 +0100

    Finished with the Project

---
+++ js/ratings.js
@@ -0,0 +1,29 @@
+
+function collect_ratings(){
+    let ratings = {average:0,
+        count:0, 
+        sum:0
+    };
+    let rating = 0;
+
+    const elements = document.querySelectorAll('.rating')
+    
+    elements.forEach(function(element){
+        rating = parseInt(element.id.replace('star', ''))
+        ratings.count += parseInt(element.value)
+        ratings.sum += parseInt(element.value*rating) 
+    });
+
+    if (ratings.count !== 0){
+        ratings.average =(ratings.sum/ratings.count)
+    }
+
+    return ratings
+}
+console.log(collect_ratings());
+
+addEventListener('change', function(){
+    const ratings = collect_ratings()
+
+    document.querySelector('#average').value = ratings.average.toFixed(2)
+})
