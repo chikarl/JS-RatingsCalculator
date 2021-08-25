aec24635eb265579daefbc26fb1cc86cde3d0d7b
chi karl <kchi8573@gmail.com>
2021-08-25 13:10:40 +0100

    Done some changes

--- js/ratings.js
+++ js/ratings.js
@@ -1,9 +1,6 @@
 
 function collect_ratings(){
-    let ratings = {average:0,
-        count:0, 
-        sum:0
-    };
+    let ratings = {average:0, count:0, sum:0 };
     let rating = 0;
 
     const elements = document.querySelectorAll('.rating')
@@ -11,7 +8,7 @@ function collect_ratings(){
     elements.forEach(function(element){
         rating = parseInt(element.id.replace('star', ''))
         ratings.count += parseInt(element.value)
-        ratings.sum += parseInt(element.value*rating) 
+        ratings.sum += parseInt(element.value)*rating
     });
 
     if (ratings.count !== 0){
